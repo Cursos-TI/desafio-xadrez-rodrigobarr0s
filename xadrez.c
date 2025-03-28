401d5f5c3e3a2bea657a583ecf53f22cdcbb8978
rodrigobarr0s <rodrigobarros.dev@gmail.com>
2025-03-28 18:46:25 -0300

    Nível Aventureiro e criado arquivo .gitignore

--- xadrez.c
+++ xadrez.c
@@ -6,7 +6,7 @@ int main() {
     for (int i = 0; i < 5; i++) {
         printf("Direita\n");
     }
-    
+
     // Movimento do Bispo (5 casas na diagonal para cima e à direita)
     printf("\nMovimento do Bispo:\n");
     int j = 0;
@@ -22,6 +22,22 @@ int main() {
         printf("Esquerda\n");
         k++;
     } while (k < 8);
+
+    // Movimento do Cavalo (2 casas para baixo e 1 para a esquerda)
+    printf("\nMovimento do Cavalo:\n");
+
+    // O cavalo se move 2 casas para baixo e 1 para a esquerda, 
+    // usando um loop aninhado
+    for (int i = 0; i < 2; i++) {
+        printf("Baixo\n");  // Primeira direção: Baixo
+    }
+
+    // Usamos o `while` para o segundo movimento do Cavalo
+    int l = 0;
+    while (l < 1) {
+        printf("Esquerda\n");  // Segunda direção: Esquerda
+        l++;
+    }
 
     return 0;
 }
